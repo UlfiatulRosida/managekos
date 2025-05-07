a4963476162d731482d987230ea650e612b5d38f
UlfiatulRosida <ulfiarosidah52@gmail.com>
2025-05-08 00:14:16 +0700

    import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:managekos/pages/home_page.dart';
 import 'package:managekos/pages/login_page.dart';
+import 'package:managekos/pages/home_page.dart';
+import 'package:managekos/pages/add_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,13 +14,12 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'managekos',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
       initialRoute: '/',
       routes: {
         '/': (context) => const LoginPage(),
         '/home': (context) => const HomePage(),
+        '/add': (context) => const AddPage(),
       },
       //home: Scaffold(body: Center(child: Text("helo"))),
     );
