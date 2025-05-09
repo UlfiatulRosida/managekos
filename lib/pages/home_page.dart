4f938a5bdaa340d630ec64c234895fe8784605b1
UlfiatulRosida <ulfiarosidah52@gmail.com>
2025-05-09 20:16:09 +0700

    body

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:managekos/pages/add_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,13 +8,29 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: Text('Daftar Kos Anda'),
         actions: [
           IconButton(
             onPressed: () => Navigator.pushNamed(context, '/add'),
             icon: Icon(Icons.add),
           ),
         ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => AddPage(),
+                  settings: RouteSettings(arguments: "tambah"),
+                ),
+              ),
+              child: Text("Tambah"),
+            ),
+            const Text(" ")
+          ],
+        ),
       ),
     );
   }
