56923ce5e1d73259d5a6a811bc20b3ed6a5ecda8
UlfiatulRosida <ulfiarosidah52@gmail.com>
2025-05-09 20:17:02 +0700

    add

---
+++ lib/pages/add_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AddPage extends StatelessWidget {
+  const AddPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Tambah Kos")),
+      body: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nama Kos',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Alamat Kos',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Simpan'),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
