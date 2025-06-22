60545df4eb661a3dbc66849609c4f593cc8dfa4e
UlfiatulRosida <ulfiarosidah52@gmail.com>
2025-06-22 21:14:34 +0700

    icon logout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,21 +9,21 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Managekos'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await Supabase.instance.client.auth.signOut();
+              if (context.mounted) {
+                Navigator.pushReplacementNamed(context, '/');
+              }
+            },
+            tooltip: "Logout",
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Selamat datang di Managekos!'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/add');
-              },
-              child: const Text('Tambah Catatan'),
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text('Selamat datang di Managekos!'),
       ),
     );
   }
